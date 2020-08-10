4bb933b9359ccb8a79ffc688c95845fe73413115
Sander Geraedts <sander.geraedts@sogeti.com>
2020-08-10 10:29:12 +0200

    added content, and changed the timeline color

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -5,7 +5,7 @@
     { date: new Date("2019-12-01"), html: `Verhuisd naar Veendam.` },
     {
       date: new Date("2019-10-01"),
-      html: `Begonnen bij <a href="https://www.dictu.nl/">DICTU</a> via <a href="https://sogeti.nl">Sogeti</a> als Full-stack Developer.`
+      html: `Begonnen bij <a href="https://www.dictu.nl/">DICTU</a> via <a href="https://sogeti.nl">Sogeti</a> als Full-stack Developer in Assen.`
     },
     {
       date: new Date("2019-7-01"),
@@ -17,10 +17,33 @@
     },
     { date: new Date("2019-6-01"), html: `Afgestudeerd van Fontys HBO ICT.` },
     {
-      date: new Date("2017-7-01"),
-      html: `Begonnen als Webdeveloper bij het voormalige Silverbee (nu <a href="https://webcommitment.nl/">Webcommitment</a>) na een succesvol afgeronde stage.`
+      date: new Date("2018-1-01"),
+      html: `Na een paar weken training de rol als coach van het quidditch team <a href="https://www.facebook.com/Lumos-Eindhoven-Quidditch-754006641437774/">Lumos Eindhoven</a> overgenomen.`
     },
-    { date: new Date("2017-2-01"), html: `Stage begonnen bij Silverbee.` }
+    {
+      date: new Date("2017-11-15"),
+      html: `Begonnen met quidditch bij Lumos Eindhoven.`
+    },
+    {
+      date: new Date("2017-7-01"),
+      html: `Officieel begonnen als Webdeveloper bij het voormalige Silverbee (nu <a href="https://webcommitment.nl/">Webcommitment</a>) na een succesvol afgeronde stage.`
+    },
+    {
+      date: new Date("2017-4-01"),
+      html: `Na 3 jaar gestopt bij <a href="https://studentaahuis.nl">Studentaanhuis.nl</a> vanwege het uitlopen van mijn contract.`
+    },
+    {
+      date: new Date("2017-2-01"),
+      html: `Stage begonnen bij Silverbee met als opdracht de nieuwe versie van de <a href="portfolio/vastgoed-bs">Vastgoed Business School</a>-website.`
+    },
+    {
+      date: new Date("2016-6-01"),
+      html: `Begonnen bij <a href="https://www.eyedevelop.nl/">EyeDevelop</a>, waar ik mijn eerste ervaring als Webdeveloper heb opgedaan.`
+    },
+    {
+      date: new Date("2014-4-01"),
+      html: `Begonnen bij Studentaanhuis.nl waar ik mensen hielp met hun computerproblemen.`
+    }
   ];
 </script>
 
